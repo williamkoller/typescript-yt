6d03822c110e6a86afa92c3912e4feae7116fe98
William Koller <wkoller25@gmail.com>
2020-03-22 23:49:46 -0300

    first commit

---
+++ scripts/hello.ts
@@ -0,0 +1,31 @@
+var p = document.createElement('h2');
+var hello: string = 'Devs vocês são fodas';
+
+p.textContent = hello;
+
+document.body.appendChild(p);
+
+let num: number = 30;
+let bool: boolean = true;
+let str: string = "A beautiful message";
+let str2: string = 'A beautiful message';
+let str3: string = `${str2} to william, its age ${num}`;
+
+console.log(str3);
+
+function func(): void {}
+
+if(bool != null){
+
+}
+
+if(bool == undefined){
+
+}
+
+if(bool){
+  let i: number;
+  for(i = 0; i < 5; i++){
+    console.log(i);
+  }
+}
